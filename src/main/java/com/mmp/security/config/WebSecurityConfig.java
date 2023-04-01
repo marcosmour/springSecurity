66dfc50d374de47ae810dc6a255eabeb50f2c59c
Marcos Moura <marcosmoura201@hotmail.com>
2023-04-01 13:57:35 -0300

    Criando a restrição para admin e user

--- src/main/java/com/mmp/security/config/WebSecurityConfig.java
+++ src/main/java/com/mmp/security/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.mmp.security.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,7 +18,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
-		http.httpBasic().and().authorizeHttpRequests().anyRequest().authenticated().and().csrf().disable();
+		http
+		.httpBasic()
+		.and()
+		.authorizeHttpRequests()
+		.antMatchers(HttpMethod.GET,"/produtos/**").permitAll()
+		.antMatchers(HttpMethod.POST,"/produtos/").hasRole("ROLE_USER")
+		.antMatchers(HttpMethod.DELETE,"/produtos/**").hasRole("ROLE_ADMIN")
+		.anyRequest()
+		.authenticated()
+		.and()
+		.csrf()
+		.disable();
 	}
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth)throws Exception {
